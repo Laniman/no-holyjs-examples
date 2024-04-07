7e6e4ee28699389d4ea91552c9a003c4f07eaca9
George Achkasov <georgij.net@gmail.com>
2024-04-07 23:47:48 +0400

    remove debounce

--- src/apps/reatom-variant/pages/github/helpers/updateCardDebounced.ts
+++
@@ -1,8 +0,0 @@
-import { putGithubCard } from '@/utils/api';
-
-import { debounce } from './debounce';
-
-const updateCard = (id: number, card: Omit<Partial<GithubCard>, 'id'>) =>
-  putGithubCard({ params: { ...card, id } });
-
-export const updateCardDebounced = debounce(updateCard, 500);
--- src/apps/reatom-variant/pages/github/model.ts
+++ src/apps/reatom-variant/pages/github/model.ts
@@ -1,8 +1,14 @@
-import { action, atom, reatomAsync, withDataAtom, withErrorAtom } from '@reatom/framework';
+import {
+  action,
+  atom,
+  concurrent,
+  reatomAsync,
+  sleep,
+  withDataAtom,
+  withErrorAtom
+} from '@reatom/framework';
 
-import { getGithubCards } from '@/utils/api';
-
-import { updateCardDebounced } from './helpers/updateCardDebounced.ts';
+import { getGithubCards, putGithubCard } from '@/utils/api';
 
 export const cardsEntriesAtom = atom<Record<string, { isDragging: boolean } & GithubCard>>(
   {},
@@ -53,36 +59,48 @@ export const setDragging = action((ctx, { id, isDragging }) => {
   selectAtom(ctx, (prev) => ({ ...prev, id: isDragging ? id : null }));
 }, 'setDragging');
 
-export const positionChange = action((ctx, payload) => {
-  const { position } = payload;
-  const { id, offset } = ctx.get(selectAtom);
-  if (!id) return;
-  const card = ctx.get(cardsEntriesAtom)[id];
-  const updatedCard = {
-    ...card,
-    position: {
-      x: position.x + offset.x - card.size.width / 2,
-      y: position.y + offset.y - card.size.height / 2
-    }
-  };
-  cardsEntriesAtom(ctx, (prev) => ({ ...prev, [id]: updatedCard }));
-  // TODO optimistic
-  updateCardDebounced(id, updatedCard);
-}, 'positionChange');
+export const positionChange = action(
+  concurrent(async (ctx, payload) => {
+    const { position } = payload;
+    const { id, offset } = ctx.get(selectAtom);
+    if (!id) return;
+    const card = ctx.get(cardsEntriesAtom)[id];
+    const updatedCard = {
+      ...card,
+      position: {
+        x: position.x + offset.x - card.size.width / 2,
+        y: position.y + offset.y - card.size.height / 2
+      }
+    };
 
-export const incrementReaction = action((ctx, payload) => {
-  const { id, reaction } = payload;
-  const entries = ctx.get(cardsEntriesAtom);
+    cardsEntriesAtom(ctx, (prev) => ({ ...prev, [id]: updatedCard }));
 
-  const updatedCard = {
-    ...entries[id],
-    reactions: {
-      ...entries[id].reactions,
-      [reaction]: entries[id].reactions[reaction] + 1
-    }
-  };
+    console.log(123);
+    await ctx.schedule(() => sleep(500));
+    console.log(4);
+    await putGithubCard({ params: { ...updatedCard, id } });
+    console.log(5);
+  }),
+  'positionChange'
+);
 
-  cardsEntriesAtom(ctx, (prev) => ({ ...prev, [id]: updatedCard }));
-  // TODO optimistic
-  updateCardDebounced(id, updatedCard);
-}, 'incrementReaction');
+export const incrementReaction = action(
+  concurrent(async (ctx, payload) => {
+    const { id, reaction } = payload;
+    const entries = ctx.get(cardsEntriesAtom);
+
+    const updatedCard = {
+      ...entries[id],
+      reactions: {
+        ...entries[id].reactions,
+        [reaction]: entries[id].reactions[reaction] + 1
+      }
+    };
+
+    cardsEntriesAtom(ctx, (prev) => ({ ...prev, [id]: updatedCard }));
+
+    await ctx.schedule(() => sleep(500));
+    await putGithubCard({ params: { ...updatedCard, id } });
+  }),
+  'incrementReaction'
+);
