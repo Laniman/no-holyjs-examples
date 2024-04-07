72970d2202e211d240565ddaabe3ff728f85eff8
George Achkasov <georgij.net@gmail.com>
2024-04-07 23:21:17 +0400

    fix theme init

--- src/apps/reatom-variant/model.ts
+++ src/apps/reatom-variant/model.ts
@@ -1,16 +1,25 @@
-import { atom, reatomAsync, withDataAtom } from '@reatom/framework';
+import { action, atom, reatomAsync, withDataAtom, withInit } from '@reatom/framework';
 import { withLocalStorage } from '@reatom/persist-web-storage';
 
 import { COOKIE } from '@/utils';
 import { getProfile } from '@/utils/api';
 
-export const themeAtom = atom('light', 'themeAtom').pipe(withLocalStorage(COOKIE.THEME));
-
-themeAtom.onChange((_, theme) => {
+const setTheme = action((_, theme) => {
   const root = window.document.documentElement;
   root.classList.remove('light', 'dark');
   root.classList.add(theme);
-});
+}, 'setTheme');
+
+export const themeAtom = atom('light', 'themeAtom').pipe(
+  withInit((ctx, init) => {
+    const theme = init(ctx);
+    setTheme(ctx, theme);
+    return theme;
+  }),
+  withLocalStorage(COOKIE.THEME)
+);
+
+themeAtom.onChange(setTheme);
 
 export const tokenAtom = atom<null | string>(null, 'tokenAtom').pipe(
   withLocalStorage(COOKIE.ACCESS_TOKEN)
